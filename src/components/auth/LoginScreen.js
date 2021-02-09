2fcd46921af758b5689f6c6d3e57c84b0b6ff06f
Karen Marlen <72042911+karen-marlen@users.noreply.github.com>
2021-02-09 13:11:50 -0300

    :art: style scss

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -2,8 +2,45 @@ import React from 'react'
 
 export const LoginScreen = () => {
     return (
-        <div>
-            <h1>LoginScreen</h1>
-        </div>
+        <>
+            <h3>Login</h3>
+
+            <form>
+                <input 
+                    type="text"
+                    placeholder="email"
+                    name="email"
+                />    
+
+                <input 
+                    type="password"
+                    placeholder="Password"
+                    name="password"
+                />
+
+                <button
+                    type="submit"
+                >
+                    Login
+                </button>
+
+                <hr />
+                
+                <div>
+                    <p>
+                        Login with social networks
+                    </p>
+
+                    <div className="google-btn">
+                        <div className="google-icon-wrapper">
+                            <img src="" alt="" className="google-icon"/>
+                        </div>
+                        <p className="btn-text">
+                            <p>Sign in with google</p>
+                        </p>
+                    </div>
+                </div>
+            </form>  
+        </>
     )
 }
