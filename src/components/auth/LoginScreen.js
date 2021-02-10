f918583ce1328c6f896eb6f277947516cf7c57f7
Karen Marlen <72042911+karen-marlen@users.noreply.github.com>
2021-02-10 11:09:34 -0300

    register login :closed_lock_with_key:

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -50,7 +50,10 @@ export const LoginScreen = () => {
                     </div>
                 </div>
 
-                <Link to="auth/register">
+                <Link 
+                    to="auth/register"
+                    className="link"
+                >
                     Create New Account
                 </Link>
             </form>  
